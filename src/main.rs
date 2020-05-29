1fa0e09bc1caca924468996d70d3d7623379f182
super-horst <super-horst@posteo.net>
2020-05-29 23:06:18 +0200

    main cleanup & framed echo server example

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,26 @@
-use futures::stream::StreamExt;
-
-use std::io::{stdout, BufWriter};
-use std::{env, error::Error, fmt, io};
+use std::error::Error;
 
 use tokio::{
     prelude::*,
     net::{TcpListener, TcpStream},
-    stream::Stream,
     time::delay_for,
 };
+use tokio_util::codec::Framed;
+use futures::{SinkExt, stream::StreamExt};
 
-use bytes::BytesMut;
 use rand_core::OsRng;
 use x25519_dalek::{EphemeralSecret, PublicKey};
 
-use futures::{Sink, SinkExt};
-use tokio_util::codec::{Decoder, Encoder, Framed, FramedWrite};
-
-use std::time::Duration;
-use std::io::prelude::*;
-
-use std::fs::File;
-
 mod messages;
+use crate::messages::Envelope;
+
 mod crypto;
 
-use self::messages::Envelope;
-use protobuf::CodedOutputStream;
-use protobuf::Message as Message_imported_for_functions;
-
 mod message_codec;
-
-use self::message_codec::encoding::*;
+use crate::message_codec::encoding::*;
 
 mod key_mgmt;
-use self::key_mgmt::{ast::*, visit::*, Interpreter};
-
+use crate::key_mgmt::*;
 
 async fn process(stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let codec = EnvelopeCodec {};
@@ -45,7 +30,7 @@ async fn process(stream: TcpStream) -> Result<(), Box<dyn Error>> {
     while let Some(message) = transport.next().await {
         match message {
             Ok(message) => {
-                println!("accept success = {:?}", message.get_name());
+                println!("accept success");
             }
             Err(e) => return Err(e.into()),
         }
@@ -59,8 +44,6 @@ async fn main() {
     let addr = "127.0.0.1:6142";
     let mut listener = TcpListener::bind(addr).await.unwrap();
 
-    // Here we convert the `TcpListener` to a stream of incoming connections
-    // with the `incoming` method.
     let server = {
         async move {
             let mut incoming = listener.incoming();
@@ -69,7 +52,7 @@ async fn main() {
                     Err(e) => eprintln!("accept failed = {:?}", e),
                     Ok(mut sock) => {
                         tokio::spawn(async move {
-                            let codec = EnvelopeCodec {};
+                            let codec = EnvelopeCodec::new();
                             let mut transport = Framed::new(sock, codec);
 
                             while let Some(msg) = transport.next().await {
@@ -93,13 +76,6 @@ async fn main() {
     server.await;
 }
 
-async fn write_data(data: &[u8]) {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
-
-    let result = stream.write_all(data).await;
-    println!("wrote to stream; success={:?}", result.is_ok());
-}
-
 /**
  * Session is a single TCP connection and exists for it's lifetime.
  *
@@ -114,7 +90,7 @@ impl Session {
         let codec = EnvelopeCodec {};
         let mut transport = Framed::new(socket, codec);
 
-        let s = Session { session: peer , transport };
+        let s = Session { session: peer, transport };
 
         let session_secret = EphemeralSecret::new(&mut OsRng);
         let session_public = PublicKey::from(&session_secret);
@@ -126,39 +102,17 @@ impl Session {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
-
-    //#[tokio::test]
-    async fn test_write_data() {
-        let mut stream = TcpStream::connect("127.0.0.1:6142").await.unwrap();
-
-        let mut rmsg = Envelope::new();
-        //rmsg.set_senderGuid("1234".to_string());
-        let data = rmsg.write_to_bytes().unwrap();
-
-        let result = stream.write_all(&data[..]).await;
-        println!("wrote to stream; success={:?}", result.is_ok());
-
-
-        delay_for(Duration::from_millis(1000)).await;
-
-
-        let mut rmsg = Envelope::new();
-        //rmsg.set_senderGuid("5678".to_string());
-        let data = rmsg.write_to_bytes().unwrap();
-
-        let result = stream.write_all(&data[..]).await;
-        println!("wrote to stream; success={:?}", result.is_ok());
-    }
+    use crate::messages::Envelope_Type;
 
     #[tokio::test]
     async fn test_echo() {
         let stream = TcpStream::connect("127.0.0.1:6142").await.unwrap();
 
-        let codec = EnvelopeCodec {};
+        let codec = EnvelopeCodec::new();
         let mut transport = Framed::new(stream, codec);
 
         let mut rmsg = Envelope::new();
-        rmsg.set_name("testingsumoah".to_string());
+        rmsg.set_field_type(Envelope_Type::RECEIPT);
 
         transport.send(rmsg).await.unwrap();
 
@@ -167,7 +121,7 @@ mod tests {
         while let Some(message) = transport.next().await {
             match message {
                 Ok(message) => {
-                    println!("echo success = {:?}", message.get_name());
+                    println!("echo success = {:?}", message.get_field_type());
                     break;
                 }
                 Err(e) => println!("{}", e),
