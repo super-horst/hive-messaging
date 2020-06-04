3962818918a8cd47a981e5d9ff145a028b6127ed
super-horst <super-horst@posteo.net>
2020-06-05 01:37:29 +0200

    update to build.rs to get rid of warnings

--- build.rs
+++ build.rs
@@ -1,17 +1,11 @@
 extern crate protobuf_codegen_pure;
 
+use protobuf_codegen_pure::*;
+
 fn main() {
-	let protobuf_gen = protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-		out_dir: "src",
-		input: &["resources/messages.proto"],
-		includes: &["resources"],
-		customize: protobuf_codegen_pure::Customize {
-			..Default::default()
-		},
-	});
-	
-    match protobuf_gen {
-        Ok(_uptime) => _uptime,
-        Err(err) => println!("protobuf code generation failed: {}", err),
-    };
+    Codegen::new()
+        .out_dir("src")
+        .inputs(&["resources/messages.proto"])
+        .includes(&["resources"])
+        .run().expect("Codegen failed.");
 }
