9163f22ca35f3d56ff8d66834ef7ef86e2b60927
super-horst <super-horst@posteo.net>
2020-09-26 21:18:36 +0200

    simplified tonic_build imports

--- hive-grpc/src/lib.rs
+++ hive-grpc/src/lib.rs
@@ -1,33 +1,22 @@
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use bytes::Bytes;
 
 use failure::Fail;
 use prost::Message;
 
-use hive_crypto::{FromBytes,
-                  CryptoError,
-                  PublicKey,
-                  CertificateInfoBundle,
-                  CertificateEncoding};
+use hive_crypto::{CertificateEncoding, CertificateInfoBundle, CryptoError, FromBytes, PublicKey};
 
 use std::hash::{Hash, Hasher};
 
 /// common protos
-pub mod common {
-    tonic::include_proto!("common");
-}
+pub mod common;
 
 /// account protos
-pub mod accounts {
-    tonic::include_proto!("accounts");
-}
+pub mod accounts;
 
 /// message protos
-pub mod messages {
-    tonic::include_proto!("messages");
-}
-
+pub mod messages;
 
 impl Eq for messages::MessageEnvelope {}
 
@@ -46,18 +35,19 @@ impl Hash for common::Peer {
     }
 }
 
-
 #[derive(Debug, Fail)]
 pub enum GrpcEncodingError {
     #[fail(display = "Decoding failed: {}", message)]
     Decoding {
         message: String,
-        #[fail(cause)] cause: prost::DecodeError,
+        #[fail(cause)]
+        cause: prost::DecodeError,
     },
     #[fail(display = "Encoding failed: {}", message)]
     Encoding {
         message: String,
-        #[fail(cause)] cause: prost::EncodeError,
+        #[fail(cause)]
+        cause: prost::EncodeError,
     },
 }
 
@@ -68,9 +58,13 @@ impl CertificateEncoding for GrpcCertificateEncoding {
     type CertificateType = common::Certificate;
 
     fn serialise_tbs(infos: &CertificateInfoBundle) -> Result<Vec<u8>, failure::Error> {
-        let expires = infos.expires().duration_since(UNIX_EPOCH)
-                           .map(|d| d.as_secs())
-                           .map_err(|e| CryptoError::Message { message: e.to_string() })?;
+        let expires = infos
+            .expires()
+            .duration_since(UNIX_EPOCH)
+            .map(|d| d.as_secs())
+            .map_err(|e| CryptoError::Message {
+                message: e.to_string(),
+            })?;
 
         let id = infos.public_key();
 
@@ -94,10 +88,12 @@ impl CertificateEncoding for GrpcCertificateEncoding {
         }
 
         let mut buf: Vec<u8> = Vec::with_capacity(tbs_cert.encoded_len());
-        tbs_cert.encode(&mut buf).map_err(|e| GrpcEncodingError::Encoding {
-            message: "failed to encode TBS certificate".to_string(),
-            cause: e,
-        })?;
+        tbs_cert
+            .encode(&mut buf)
+            .map_err(|e| GrpcEncodingError::Encoding {
+                message: "failed to encode TBS certificate".to_string(),
+                cause: e,
+            })?;
 
         Ok(buf)
     }
@@ -109,50 +105,49 @@ impl CertificateEncoding for GrpcCertificateEncoding {
         };
 
         let mut buf: Vec<u8> = Vec::with_capacity(cert.encoded_len());
-        cert.encode(&mut buf).map_err(|e| GrpcEncodingError::Encoding {
-            message: "failed to encode certificate".to_string(),
-            cause: e,
-        })?;
+        cert.encode(&mut buf)
+            .map_err(|e| GrpcEncodingError::Encoding {
+                message: "failed to encode certificate".to_string(),
+                cause: e,
+            })?;
 
         Ok(buf)
     }
 
-    fn decode_partial(serialised: common::Certificate)
-                      -> Result<(hive_crypto::Certificate, Option<common::Certificate>), failure::Error> {
+    fn decode_partial(
+        serialised: common::Certificate,
+    ) -> Result<(hive_crypto::Certificate, Option<common::Certificate>), failure::Error> {
         let buf = Bytes::from(serialised.certificate.to_vec());
-        let tbs_cert = common::certificate::TbsCertificate::decode(buf)
-            .map_err(|e| GrpcEncodingError::Decoding {
+        let tbs_cert = common::certificate::TbsCertificate::decode(buf).map_err(|e| {
+            GrpcEncodingError::Decoding {
                 message: "failed to decode certificate".to_string(),
                 cause: e,
-            })?;
+            }
+        })?;
 
         let signed_identity = PublicKey::from_bytes(&tbs_cert.identity[..])?;
 
         let expiration = SystemTime::UNIX_EPOCH
             .checked_add(Duration::from_secs(tbs_cert.expires))
             .ok_or(CryptoError::Message {
-                message: format!("is invalid system time '{}'", tbs_cert.expires)
+                message: format!("is invalid system time '{}'", tbs_cert.expires),
             })?;
 
-        let cert_info = CertificateInfoBundle::new(signed_identity,
-                                                   expiration,
-                                                   tbs_cert.uuid,
-                                                   None);
+        let cert_info =
+            CertificateInfoBundle::new(signed_identity, expiration, tbs_cert.uuid, None);
 
-        let cert = hive_crypto::Certificate::new(serialised.certificate,
-                                                 serialised.signature,
-                                                 cert_info);
+        let cert =
+            hive_crypto::Certificate::new(serialised.certificate, serialised.signature, cert_info);
 
         Ok((cert, tbs_cert.signer))
     }
 
     fn deserialise(bytes: Vec<u8>) -> Result<common::Certificate, failure::Error> {
         let buf = Bytes::from(bytes);
-        let cert = common::Certificate::decode(buf)
-            .map_err(|e| GrpcEncodingError::Decoding {
-                message: "failed to decode certificate".to_string(),
-                cause: e,
-            })?;
+        let cert = common::Certificate::decode(buf).map_err(|e| GrpcEncodingError::Decoding {
+            message: "failed to decode certificate".to_string(),
+            cause: e,
+        })?;
 
         Ok(cert)
     }
