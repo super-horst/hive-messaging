b390c1de50e75086509012e1cafa2319adf09f9d
super-horst <super-horst@posteo.net>
2020-09-26 21:16:36 +0200

    git ignore

--- hive-grpc/build.rs
+++ hive-grpc/build.rs
@@ -2,6 +2,7 @@ extern crate tonic_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .out_dir("./src/")
         .compile(
             &["proto/accounts_svc.proto", "proto/messages_svc.proto"],
             &["proto"],
