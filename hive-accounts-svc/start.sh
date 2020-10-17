e34e665369c605a58fbbfce1097c478f5342a8d8
super-horst <super-horst@posteo.net>
2020-10-18 00:56:58 +0200

    refactored libs into shared commons

--- hive-accounts-svc/start.sh
+++ hive-accounts-svc/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export CONFIG='{"port":8080,"loglevel":"debug","db_config":{"host":"172.17.0.2","port":5432,"user":"postgres","password":"docker","dbname":"postgres","ssl_mode":true}}'
+export CONFIG='{"port":8080,"loglevel":"debug","db_config":{"host":"accounts_db","port":5432,"user":"postgres","password":"accounts_db","dbname":"common","ssl_mode":true},"certificate":"./certs", "key":"./privates"}'
 cargo run
