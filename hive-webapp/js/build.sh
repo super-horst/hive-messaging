d1f7f6b244be4d4ec23de53e83a6fcbe848117f1
super-horst <super-horst@posteo.net>
2020-10-18 19:25:19 +0200

    minimise js scope

--- hive-webapp/js/build.sh
+++ hive-webapp/js/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir src/generated
+mkdir src
 
 docker build -t protoc_container .
 
@@ -11,5 +11,7 @@ export SRC_DIR=/src
 docker run --env PROTOS --env PROTO_DIR --env SRC_DIR  \
   --mount type=bind,src=$(pwd)/../../hive-commons/proto,dst=$PROTO_DIR \
   --mount type=bind,src=$(pwd)/src,dst=$SRC_DIR \
-  protoc_container:latest ./build_js_app.sh
- # -ti protoc_container:latest bash
+  protoc_container:latest \
+  protoc -I="$PROTO_DIR" $PROTOS \
+    --js_out=import_style=commonjs:"$SRC_DIR" \
+    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:"$SRC_DIR"
