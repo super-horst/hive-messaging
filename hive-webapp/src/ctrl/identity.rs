1b7d1a92d72ac2a02ffdada8d2d152c4939bd06d
super-horst <super-horst@posteo.net>
2021-03-24 21:27:37 +0100

    refactored to resolve compiler issues

--- hive-webapp/src/ctrl/identity.rs
+++ hive-webapp/src/ctrl/identity.rs
@@ -11,48 +11,52 @@ use yew::Callback;
 use log::*;
 use serde::{Deserialize, Serialize};
 
-use hive_commons::crypto::{Certificate, CryptoError, KeyAgreement, PrivateKey, PublicKey, Signer, CertificateFactory};
+use hive_commons::crypto::{
+    Certificate, CertificateFactory, CryptoError, KeyAgreement, PrivateKey, PublicKey, Signer,
+};
 use hive_commons::protocol;
 
 use hive_commons::model::{common, Decodable};
 
-use crate::bindings::{common_bindings, accounts_svc_bindings};
-use crate::transport::ConnectionManager;
+use crate::bindings::{accounts_svc_bindings, common_bindings};
 use crate::ctrl::{ControllerError, StorageController};
+use crate::transport::ConnectionManager;
 
 const IDENTITY_KEY: &'static str = "hive.core.identity";
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct IdentityModel {
+    key: PrivateKey,
+    state: IdentityState,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum IdentityState {
-    New {
-        key: PrivateKey,
-    },
-    Acknowledged {
-        key: PrivateKey,
-        certificate: Certificate,
-    },
-    Initialised {
-        key: PrivateKey,
-        certificate: Certificate,
-        pre_keys: protocol::PrivatePreKeys,
-    },
+    New,
+    Acknowledged { certificate: Certificate },
+    Initialised { certificate: Certificate, pre_keys: protocol::PrivatePreKeys },
 }
 
 #[derive(Clone)]
 pub struct IdentityController {
     storage: StorageController,
     transport: ConnectionManager,
+    key: PrivateKey,
     state: Arc<RwLock<RefCell<IdentityState>>>,
 }
 
 impl IdentityController {
-    pub fn new(on_error: Callback<String>, storage: StorageController,
-               transport: ConnectionManager, ) -> Result<IdentityController, ControllerError> {
-        match storage.load::<IdentityState>(IDENTITY_KEY) {
-            Ok(state) => Ok(IdentityController {
+    pub fn new(
+        on_error: Callback<String>,
+        storage: StorageController,
+        transport: ConnectionManager,
+    ) -> Result<IdentityController, ControllerError> {
+        match storage.load::<IdentityModel>(IDENTITY_KEY) {
+            Ok(model) => Ok(IdentityController {
                 storage,
                 transport,
-                state: Arc::new(RwLock::new(RefCell::new(state))),
+                key: model.key,
+                state: Arc::new(RwLock::new(RefCell::new(model.state))),
             }),
             Err(cause) => {
                 match cause {
@@ -68,13 +72,14 @@ impl IdentityController {
                     }
                 })?;
 
-                let state = IdentityState::New { key };
+                let model = IdentityModel { key, state: IdentityState::New };
 
-                storage.store(IDENTITY_KEY, &state)?;
+                storage.store(IDENTITY_KEY, &model)?;
                 let ctrl = IdentityController {
                     storage,
                     transport,
-                    state: Arc::new(RwLock::new(RefCell::new(state))),
+                    key: model.key,
+                    state: Arc::new(RwLock::new(RefCell::new(model.state))),
                 };
 
                 ctrl.identity_maintenance(on_error);
@@ -116,7 +121,8 @@ impl IdentityController {
         debug!("Creating new account");
         let challenge = protocol::sign_challenge(&self).unwrap();
 
-        let unsig_challenge = common::signed_challenge::Challenge::decode(challenge.challenge.clone()).unwrap();
+        let unsig_challenge =
+            common::signed_challenge::Challenge::decode(challenge.challenge.clone()).unwrap();
         debug!("Challenge timestamp {}", unsig_challenge.timestamp);
 
         let bound_challenge = common_bindings::SignedChallenge::new();
@@ -138,12 +144,38 @@ impl IdentityController {
         self.incoming_certificate(certificate).await
     }
 
+    async fn incoming_certificate(&self, certificate: Certificate) -> Result<(), ControllerError> {
+        {
+            let write_state = self.state.write().map_err(|cause| ControllerError::Message {
+                message: "Locking failed".to_string(),
+            })?;
+
+            write_state.replace_with(|state| match state {
+                IdentityState::New | IdentityState::Acknowledged { .. } => {
+                    IdentityState::Acknowledged { certificate }
+                }
+                IdentityState::Initialised { certificate: _, pre_keys } => {
+                    IdentityState::Initialised { certificate, pre_keys: pre_keys.clone() }
+                }
+            });
+        };
+
+        // TODO blocking in async
+        self.store()?;
+
+        Ok(())
+    }
+
     async fn publish_pre_key_bundle(&self) -> Result<(), ControllerError> {
         // TODO error handling
         debug!("Publishing new pre key bundle");
 
-        let (pre_keys, privates) = protocol::create_pre_key_bundle(&self)
-            .map_err(|cause| ControllerError::CryptographicError { message: "Failed to create pre key bundle".to_string(), cause })?;
+        let (pre_keys, privates) = protocol::create_pre_key_bundle(&self).map_err(|cause| {
+            ControllerError::CryptographicError {
+                message: "Failed to create pre key bundle".to_string(),
+                cause,
+            }
+        })?;
 
         let bound_pre_keys: common_bindings::PreKeyBundle = pre_keys.into();
 
@@ -156,57 +188,26 @@ impl IdentityController {
         self.pre_keys_accepted(privates).await
     }
 
-    async fn incoming_certificate(&self, certificate: Certificate) -> Result<(), ControllerError> {
-        {
-            let write_state = self.state.write().map_err(|cause| ControllerError::Message {
-                message: "Locking failed".to_string(),
-            })?;
-
-            write_state.replace_with(|state| match state {
-                IdentityState::New { key } | IdentityState::Acknowledged { key, .. } => {
-                    IdentityState::Acknowledged {
-                        key: key.clone(),
-                        certificate,
-                    }
-                }
-                IdentityState::Initialised {
-                    key,
-                    certificate: _,
-                    pre_keys,
-                } => IdentityState::Initialised {
-                    key: key.clone(),
-                    certificate,
-                    pre_keys: pre_keys.clone(),
-                },
-            });
-        };
-
-        // TODO blocking in async
-        self.store()?;
-
-        Ok(())
-    }
-
-    async fn pre_keys_accepted(&self, private_pre_keys: protocol::PrivatePreKeys) -> Result<(), ControllerError> {
+    async fn pre_keys_accepted(
+        &self,
+        private_pre_keys: protocol::PrivatePreKeys,
+    ) -> Result<(), ControllerError> {
         {
             let write_state = self.state.write().map_err(|cause| ControllerError::Message {
                 message: "Locking failed".to_string(),
             })?;
 
             let update = match write_state.deref().borrow().deref() {
-                IdentityState::New { .. } => Err(ControllerError::InvalidState {
+                IdentityState::New => Err(ControllerError::InvalidState {
                     message: "Did not receive a certificate before pre keys".to_string(),
                 }),
-                IdentityState::Acknowledged { key, certificate }
-                | IdentityState::Initialised {
-                    key,
-                    certificate,
-                    pre_keys: _,
-                } => Ok(IdentityState::Initialised {
-                    key: *key,
-                    certificate: certificate.clone(),
-                    pre_keys: private_pre_keys,
-                }),
+                IdentityState::Acknowledged { certificate }
+                | IdentityState::Initialised { certificate, pre_keys: _ } => {
+                    Ok(IdentityState::Initialised {
+                        certificate: certificate.clone(),
+                        pre_keys: private_pre_keys,
+                    })
+                }
             }?;
 
             write_state.replace(update);
@@ -220,13 +221,11 @@ impl IdentityController {
 
     pub fn certificate(&self) -> Option<Certificate> {
         return match self.state.read() {
-            Ok(guard) => {
-                match guard.deref().borrow().deref() {
-                    IdentityState::New { .. } => None,
-                    IdentityState::Acknowledged { certificate, .. }
-                    | IdentityState::Initialised { certificate, .. } => Some(certificate.clone()),
-                }
-            }
+            Ok(guard) => match guard.deref().borrow().deref() {
+                IdentityState::New { .. } => None,
+                IdentityState::Acknowledged { certificate, .. }
+                | IdentityState::Initialised { certificate, .. } => Some(certificate.clone()),
+            },
             Err(cause) => {
                 error!("Failed to lock: {}", cause);
                 None
@@ -241,27 +240,22 @@ impl IdentityController {
     fn store(&self) -> Result<(), ControllerError> {
         let read_state = self
             .state
-            .read().map_err(|cause| ControllerError::Message {
-            message: "Locking failed".to_string(),
-        })?;
+            .read()
+            .map_err(|cause| ControllerError::Message { message: "Locking failed".to_string() })?;
 
-        let store_result = self
-            .storage
-            .store(IDENTITY_KEY, read_state.borrow().deref());
+        let model = IdentityModel {
+            key: self.key.clone(),
+            state: read_state.deref().borrow().deref().clone(),
+        };
+
+        let store_result = self.storage.store(IDENTITY_KEY, &model);
         store_result
     }
 }
 
 impl protocol::KeyAccess for IdentityController {
     fn identity_access(&self) -> &PrivateKey {
-        // TODO error handling
-        let read_state = self.state.read().unwrap();
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => &key,
-        };
+        &self.key
     }
 
     fn pre_key_access(&self) -> &PrivateKey {
@@ -275,75 +269,32 @@ impl protocol::KeyAccess for IdentityController {
 
 impl Signer for &IdentityController {
     fn public_key(&self) -> &PublicKey {
-        // TODO error handling
-        let read_state = self.state.read().unwrap();
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => &key.public_key(),
-        };
+        &self.key.public_key()
     }
 
     fn sign(&self, data: &[u8]) -> Result<Vec<u8>, CryptoError> {
-        let read_state = self.state.read().map_err(|cause| CryptoError::Message {
-            message: "Locking failed".to_string(),
-        })?;
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => key.sign(data),
-        };
+        self.key.sign(data)
     }
 }
 
 impl Signer for IdentityController {
     fn public_key(&self) -> &PublicKey {
-        // TODO error handling
-        let read_state = self.state.read().unwrap();
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => key.public_key(),
-        };
+        &self.key.public_key()
     }
 
     fn sign(&self, data: &[u8]) -> Result<Vec<u8>, CryptoError> {
-        let read_state = self.state.read().map_err(|cause| CryptoError::Message {
-            message: "Locking failed".to_string(),
-        })?;
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => key.sign(data),
-        };
+        self.key.sign(data)
     }
 }
 
 impl KeyAgreement for IdentityController {
     fn agree(&self, public: &PublicKey) -> [u8; 32] {
-        let read_state = self
-            .state
-            .read()
-            .map_err(|cause| CryptoError::Message {
-                message: "Locking failed".to_string(),
-            })
-            .unwrap();
-
-        return match read_state.deref().borrow().deref() {
-            IdentityState::New { key }
-            | IdentityState::Acknowledged { key, .. }
-            | IdentityState::Initialised { key, .. } => key.agree(public),
-        };
+        self.key.agree(public)
     }
 }
 
 #[cfg(test)]
 mod identity_ctrl_tests {
-
     #[test]
     fn test_ing() {}
 }
